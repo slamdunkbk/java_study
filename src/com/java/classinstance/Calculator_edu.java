bd0864fe324024837c2105d77e337201bc35dc8f
slamdunkbk <slamdunkbk@naver.com>
2015-09-12 01:09:12 +0900

    2015/9/12 Class and object

---
+++ src/com/java/classinstance/Calculator_edu.java
@@ -0,0 +1,35 @@
+package com.java.classinstance;
+
+class Calculator {
+	// PI 변수는 클래스의 맴버 변수이다
+	static double PI = 3.14;
+	// left, right 변수는 인스턴스의 맴버 변수이다
+	int left, right;
+
+	public void setOprands(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	public void sum() {
+		System.out.println(this.left + this.right);
+	}
+
+	public void avg() {
+		System.out.println((this.left + this.right) / 2);
+	}
+}
+
+public class Calculator_edu {
+
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator();
+		System.out.println(c1.PI);
+
+		Calculator c2 = new Calculator();
+		System.out.println(c2.PI);
+
+		System.out.println(Calculator.PI);
+	}
+
+}
---
+++ src/com/java/object/Claculator_edu.java
@@ -0,0 +1,30 @@
+package com.java.object;
+
+public class Claculator_edu {
+
+	public static void sum(int left, int right) {
+		System.out.println(left + right);
+	}
+
+	public static void avg(int left, int right) {
+		System.out.println((left + right) / 2);
+	}
+
+	public static void main(String[] args) {
+		int left, right;
+
+		// 변수값 선언 및 sum, avg 매소드 실행하여 값 출력
+		left = 10;
+		right = 20;
+
+		sum(left, right);
+		avg(left, right);
+
+		// 변수값 다시 선언 및 매소드 값 출력
+		left = 20;
+		right = 40;
+
+		sum(left, right);
+		avg(left, right);
+	}
+}
